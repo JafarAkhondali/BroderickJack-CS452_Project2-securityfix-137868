15f8445a495d26f4da775f686f9e7c26b5090ff7
BroderickJack <broderick.jack13@gmail.com>
2019-04-21 15:06:37 -0400

    Got the coke bottle to be visible with a few problems ...

--- Project2.js
+++ Project2.js
@@ -1,13 +1,11 @@
 /* Jack Broderick, Alison Davis */
 /* April 10, 2019 */
 /* CS468 Project 2 */
-var vertices;
-var vertices4 = [];
-var indices = [];
-var vertexNormals = [];
+
 var gl;
+var numVertices;
+var numTriangles;
 var myShaderProgram;
-var geometry;
 
 /* Global Uniforms */
 var MUniform;
@@ -43,17 +41,19 @@ var AlphaUniform; /* Uniform for sending the alpha value for shine */
 var P_ortho;
 var P_persp;
 
-var ToggleSpecularUniform;
-
 /* 1: Orthographic 0: Perspective */
 var projection_select; /* Selects Orthographic vs. Perspecvice Projection */
 var source1;
 var source2;
 var toggleSpecular;
-var numTriangles;
+
+/* Create a global variable to pass the vertices between functions */
+var vert = [];
+var vertices = [];
+var indices = [];
+var vertexNormals = [];
 
 function initGL() {
-    /* This function is called when the webpage is loaded */
     var canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
@@ -65,6 +65,27 @@ function initGL() {
 
     myShaderProgram = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( myShaderProgram );
+
+    /* Get all of the uniform locations */
+    MUniform = gl.getUniformLocation(myShaderProgram, "modelview");
+    PUniform = gl.getUniformLocation(myShaderProgram, "projection");
+    MVitUniform = gl.getUniformLocation(myShaderProgram, "modelviewInverseTranspose");
+    PosUniform = gl.getUniformLocation(myShaderProgram, "p0");
+    PosUniform2 = gl.getUniformLocation(myShaderProgram, "p02");
+    IaUniform = gl.getUniformLocation(myShaderProgram, "Ia");
+    IdUniform = gl.getUniformLocation(myShaderProgram, "Id");
+    IsUniform = gl.getUniformLocation(myShaderProgram, "Is");
+    IaUniform2 = gl.getUniformLocation(myShaderProgram, "Ia2");
+    IdUniform2 = gl.getUniformLocation(myShaderProgram, "Id2");
+    IsUniform2 = gl.getUniformLocation(myShaderProgram, "Is2");
+    KaUniform = gl.getUniformLocation(myShaderProgram, "ka");
+    KdUniform = gl.getUniformLocation(myShaderProgram, "kd");
+    KsUniform = gl.getUniformLocation(myShaderProgram, "ks");
+    UUniform = gl.getUniformLocation(myShaderProgram, "u_spotlight");
+    Source1Uniform = gl.getUniformLocation(myShaderProgram, "source1");
+    Source2Uniform = gl.getUniformLocation(myShaderProgram, "source2");
+    AlphaUniform = gl.getUniformLocation(myShaderProgram, "alpha");
+    ToggleSpecularUniform = gl.getUniformLocation(myShaderProgram, "toggleSpecular");
 
     // Step 1: Position the camera using the look at method
 
@@ -126,133 +147,6 @@ function initGL() {
     gl.uniformMatrix4fv(MUniform, false, M);
     gl.uniformMatrix4fv(MVitUniform, false, modelviewInverseTranspose);
 
-    /* Call the function to load the object */
-    load_object();
-}
-
-function load_object() {
-    // instantiate a loader
-    var loader = new THREE.OBJLoader();
-
-    // load a resource
-    loader.load(
-    	// resource URL
-    	'coke_bottle.OBJ',
-
-    	// called when resource is loaded
-    	function ( object ) {
-            /* We get the vertices of all of the points */
-            /* Each face is stored in order, so some points are repeated */
-            // vertices = object.children[0].geometry.attributes.position.array;
-            // vertexNormals = object.children[0].geometry.attributes.normal.array;
-            // numTriangles = Math.floor(vertices/9.0);
-
-            // console.log(vertices);
-            // console.log(object.children[0].geometry);
-            /* Create a geometry object from a buffer geometry */
-            geometry = new THREE.Geometry().fromBufferGeometry( object.children[0].geometry );
-            // console.log("Geometry: ", geometry);
-
-            /* Get the vertices from the geometry object */
-            // console.log(geometry.vertices);
-
-            vertices = geometry.vertices;
-
-            // console.log(Math.max.apply(null,vertices));
-            // console.log(Math.min(vertices));
-
-            // console.log(object.children[0].geometry);
-
-            send_vertices();
-    	},
-    	// called when loading is in progresses
-    	function ( xhr ) {
-    		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-    	},
-    	// called when loading has errors
-    	function ( error ) {
-
-    		console.log( 'An error happened' );
-
-    	}
-    );
-
-}
-
-function send_vertices() {
-    /* We need to add 1.0 to all of the vertices because it is going to be looking for a vec4 in the vertex shader for position */
-    var i = 0;
-    var j = 0;
-
-    var faces = geometry.faces;
-
-    for(i = 0; i < vertices.length; i++) {
-        vertices4.push(vertices[i].x);
-        vertices4.push(vertices[i].y);
-        vertices4.push(vertices[i].z);
-        vertices4.push(1.0);
-        indices.push(i); /* The vertices for each face are supposed to be repeated so they should all be in order */
-    }
-
-    // console.log("vertices4: ", vertices4);
-
-    /* Now we need to create a vector for the indices */
-    var faces = geometry.faces;
-    // console.log(faces);
-    // console.log(faces[1].a);
-    var v_n = [];
-    for(i = 0; i < faces.length; i++) {
-        indices.push(faces[i].a);
-        // console.log(indices);
-        indices.push(faces[i].b);
-        indices.push(faces[i].c);
-
-        /* We need to get the vertex normal for each of the vertices in the face */
-        v_n[faces[i].a] = faces[i].vertexNormals[0];
-        v_n[faces[i].b] = faces[i].vertexNormals[1];
-        v_n[faces[i].c] = faces[i].vertexNormals[2];
-    }
-
-    /* We need to get the values from the vertexNormals */
-    for(i = 0; i < v_n.length; i++) {
-        vertexNormals.push(v_n[i].x);
-        vertexNormals.push(v_n[i].y);
-        vertexNormals.push(v_n[i].z);
-    }
-
-    // console.log(vertexNormals);
-    // console.log(flatten(vertexNormals));
-    // console.log("indices: ", indices);
-
-    // console.log(geometry);
-
-    /* Get the vertex normals from the geometry */
-    /* We need to loop through all of the vertex normals in the faces */
-
-
-    /* Create the index buffer */
-    var indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
-    /* Create, bind, and send data to the buffer for the vertices */
-    var vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices4), gl.STATIC_DRAW);
-
-    var vertexPosition = gl.getAttribLocation(myShaderProgram, "vertexPosition");
-    gl.vertexAttribPointer(vertexPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vertexPosition);
-
-    /* Create a buffer for the vertex normals */
-    var vertexNormal = gl.getAttribLocation(myShaderProgram, "vertexNormal");
-    var normalsBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexNormals), gl.STATIC_DRAW);
-    gl.vertexAttribPointer(vertexNormal, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vertexNormal);
-
     // Set up the first light source and send the variables
     // to the shader program (NEEDS CODE, VARIABLES DEPEND ON LIGHT TYPE)
     /* Point light source at point p */
@@ -280,9 +174,9 @@ function send_vertices() {
     gl.uniform3f(IaUniform, Ia[0], Ia[1], Ia[2]);
     gl.uniform3f(IdUniform, Id[0], Id[1], Id[2]);
     gl.uniform3f(IsUniform, Is[0], Is[1], Is[2]);
-    gl.uniform3f(Ia2Uniform, Ia2[0], Ia2[1], Ia2[2]);
-    gl.uniform3f(Id2Uniform, Id2[0], Id2[1], Id2[2]);
-    gl.uniform3f(Is2Uniform, Is2[0], Is2[1], Is2[2]);
+    gl.uniform3f(IaUniform2, Ia2[0], Ia2[1], Ia2[2]);
+    gl.uniform3f(IdUniform2, Id2[0], Id2[1], Id2[2]);
+    gl.uniform3f(IsUniform2, Is2[0], Is2[1], Is2[2]);
 
     /* Setup the object specific variables */
     var ka = vec3(0.6, 0.6, 0.1);
@@ -309,17 +203,17 @@ function send_vertices() {
     // Step 2: Set up orthographic and perspective projections
 
     // Define left plane
-    var left = -150.0;
+    var left = -250.0;
     // Define right plane
-    var right = 150.0;
+    var right = 250.0;
     // Define top plane
-    var top = 150.0;
+    var top = 400.0;
     // Define bottom plane
-    var bottom = -150.0;
+    var bottom = -400.0;
     // Define near plane
-    var near = 50.0;
+    var near = 10.0;
     // Define far plane
-    var far = 150.0;
+    var far = 400.0;
 
     /* Set up orthographic projection matrix P_orth using above planes */
     P_ortho = [2.0/ (right-left),
@@ -361,8 +255,186 @@ function send_vertices() {
     // orthographic and perspective projections
     projection_select = 1; /* 1: Orthographic 0: Perspective */
 
+    /* We need to load the object */
+    load_object();
+};
+
+
+function load_object() {
+    // instantiate a loader
+    var loader = new THREE.OBJLoader();
+
+    // load a resource
+    loader.load(
+    	// resource URL
+    	'coke_bottle.OBJ',
+
+    	// called when resource is loaded
+    	function ( object ) {
+            /* We get the vertices of all of the points */
+
+            /* Create a geometry object from a buffer geometry */
+            geometry = new THREE.Geometry().fromBufferGeometry( object.children[0].geometry );
+
+            /* Get the vertices from the geometry object */
+            vert = geometry.vertices;
+
+            /* Next, call a function to process the loaded geometry object */
+            process_object();
+
+    	},
+    	// called when loading is in progresses
+    	function ( xhr ) {
+    		// console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    	},
+    	// called when loading has errors
+    	function ( error ) {
+
+    		console.log( 'An error happened' );
+
+    	}
+    );
+
+}
+
+function process_object() {
+    /* We need to add 1.0 to all of the vertices because it is going to be looking for a vec4 in the vertex shader for position */
+    var i = 0;
+    var j = 0;
+
+    var faces = geometry.faces;
+
+    for(i = 0; i < vert.length; i++) {
+        vertices.push(vert[i].x);
+        vertices.push(vert[i].y);
+        vertices.push(vert[i].z);
+        vertices.push(1.0);
+    }
+
+    /* Now we need to create a vector for the indices */
+    var faces = geometry.faces;
+    // console.log(faces);
+    // console.log(faces[1].a);
+    var v_n = [];
+    for(i = 0; i < faces.length; i++) {
+        indices.push(faces[i].a);
+        indices.push(faces[i].b);
+        indices.push(faces[i].c);
+
+        /* We need to get the vertex normal for each of the vertices in the face */
+        v_n[faces[i].a] = faces[i].vertexNormals[0];
+        v_n[faces[i].b] = faces[i].vertexNormals[1];
+        v_n[faces[i].c] = faces[i].vertexNormals[2];
+    }
+
+    numTriangles = faces.length;
+
+    /* We need to get the values from the vertexNormals */
+    for(i = 0; i < v_n.length; i++) {
+        vertexNormals.push(v_n[i].x);
+        vertexNormals.push(v_n[i].y);
+        vertexNormals.push(v_n[i].z);
+    }
+
+    /* We now need to send the values via buffers */
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    /* Create a buffer for the vertex normals */
+    var vertexNormal = gl.getAttribLocation(myShaderProgram, "vertexNormal");
+    var normalsBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexNormals), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(vertexNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertexNormal);
+
+    /* Create, bind, and send data to the buffer for the vertices */
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var vertexPosition = gl.getAttribLocation(myShaderProgram, "vertexPosition");
+    gl.vertexAttribPointer(vertexPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertexPosition);
+
     // render the object
     drawObject();
+}
+
+// FOLLOWING CODE SKELETON FOR getFaceNormals() NEEDS TO BE COMPLETED
+function getFaceNormals( vertices, indexList, numTriangles ) {
+    // array of face normals
+    var faceNormals = [];
+    var faceNormal = [];
+
+    // Following lines iterate over triangles
+    for (var i = 0; i < numTriangles; i++) {
+        // Following lines give you three vertices for each face of the triangle
+        var p0 = vec3( vertices[indexList[3*i]][0],
+                      vertices[indexList[3*i]][1],
+                      vertices[indexList[3*i]][2]);
+
+        var p1 = vec3( vertices[indexList[3*i+1]][0],
+                      vertices[indexList[3*i+1]][1],
+                      vertices[indexList[3*i+1]][2]);
+
+        var p2 = vec3( vertices[indexList[3*i+2]][0],
+                      vertices[indexList[3*i+2]][1],
+                      vertices[indexList[3*i+2]][2]);
+
+        // Calculate vector from p0 to p1 ( use subtract function in MV.js, NEEDS CODE )
+        var v1 = subtract(p1, p0);
+        // Calculate vector from p0 to p2 ( use subtract function, NEEDS CODE )
+        var v2 = subtract(p2, p0);
+        // Calculate face normal as the cross product of the above two vectors
+        // (use cross function in MV.js, NEEDS CODE )
+        var n = cross(v1, v2);
+        // normalize face normal (use normalize function in MV.js, NEEDS CODE)
+        faceNormal = normalize(n);
+        // Following line pushes the face normal into the array of face normals
+        faceNormals.push( faceNormal );
+    }
+
+    // Following line returns the array of face normals
+    return faceNormals;
+}
+
+// FOLLOWING CODE SKELETON FOR getVertexNormals() NEEDS TO BE COMPLETED
+function getVertexNormals( vertices, indexList, faceNormals, numVertices, numTriangles ) {
+    var vertexNormals = [];
+
+    // Iterate over all vertices
+    for ( var j = 0; j < numVertices; j++) {
+
+        // Initialize the vertex normal for the j-th vertex
+        var vertexNormal = vec3( 0.0, 0.0, 0.0 );
+
+        // Iterate over all the faces to find if this vertex belongs to
+        // a particular face
+
+        for ( var i = 0; i < numTriangles; i++ ) {
+
+            // The condition of the following if statement should check
+            // if the j-th vertex belongs to the i-th face
+                // console.log("i: ", i);
+                // console.log("indexList[0-3]: ", indexList.slice(i,i+3));
+                // console.log("IndexList: ", indexList);
+            if ( indexList.slice(i*3, (i*3)+3).includes(j) ) { // NEEDS CODE IN PARENTHESES
+                vertexNormal = add(vertexNormal, faceNormals[i]);
+            }
+
+        }
+
+        /* Normalize the vertex normal */
+        vertexNormal = normalize(vertexNormal);
+
+        // Following line pushes the vertex normal into the vertexNormals array
+        vertexNormals.push( vertexNormal );
+    }
+
+    return vertexNormals;
 }
 
 function set_orthographic() {
